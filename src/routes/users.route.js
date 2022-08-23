99db8c68b1120e0f5b1f9f78d80dc9b127dc0bf0
Leo Ruiz <ruizruizleo@hotmail.com>
2022-08-22 21:13:50 -0300

    Aula 16

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const userController = require("../controller/user.controller");
 
-router.get("/", userController.findAllUserController);
+router.get("/findAll", userController.findAllUserController);
 router.get("/findByEmail", userController.findByEmailUserController);
 
-router.post("/", userController.createUserController);
+router.post("/create", userController.createUserController);
 
 module.exports = router;
